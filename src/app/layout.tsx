23213502a316085634928f7d9f5eadb827f2883c
Ghost Guin <ghostguin88@gmail.com>
2025-08-18 19:11:32 +0000

    can we change the colors scheme to have native ios light and dark colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,10 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#A050BE',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+  ],
   width: 'device-width',
   initialScale: 1,
   viewportFit: 'cover',
